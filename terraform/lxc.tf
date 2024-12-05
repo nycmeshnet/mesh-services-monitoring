cc2b4de54d7a2778027bc0dd8bb071b6d1a17b89
james-otten <jamesotten1@gmail.com>
2024-12-04 23:11:14 -0500

    init

---
+++ terraform/lxc.tf
@@ -0,0 +1,27 @@
+resource "proxmox_lxc" "monitoring_host" {
+  target_node  = var.proxmox_node
+  hostname     = var.hostname
+  description  = "Monitoring host managed IaC via https://github.com/nycmesh/mesh-services-monitoring"
+  ostemplate   = var.system_image
+  password     = var.local_password
+  cores        = 2
+  unprivileged = true
+  start        = true
+  onboot       = true
+
+  ssh_public_keys = file("${path.module}/mesh_services_monitoring.pub")
+
+  rootfs {
+    storage = var.proxmox_storage_location
+    size    = "4G"
+  }
+
+  network {
+    name   = "eth0"
+    bridge = var.vm_nic
+    ip     = "${var.vm_mgt_ip}/${var.internal_host_identifier}"
+    gw     = var.vm_mgt_default_gateway
+  }
+
+  tags = "monitoring,managed_by_iac"
+}
