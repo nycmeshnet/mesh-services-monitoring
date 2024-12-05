3f61bdc06422d4ff5437008b63994c49212334f1
james-otten <jamesotten1@gmail.com>
2024-12-04 23:43:39 -0500

    updates

--- terraform/ansible.tf
+++ terraform/ansible.tf
@@ -12,5 +12,5 @@ resource "ansible_group" "monitor_mgt_group" {
 
 resource "ansible_host" "monitor_mgt" {
   name = var.vm_mgt_ip
-  groups = [ansible_group.lb.name]
+  groups = [ansible_group.monitor_mgt_group.name]
 }
